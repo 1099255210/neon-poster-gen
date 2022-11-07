8cf9fc8f4993925055566f03f290781c1d38a636
1099255210 <1099255210@qq.com>
2022-11-07 12:48:15 +0800

    modified some description.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ libraqm(https://github.com/HOST-Oman/libraqm) is needed.
 If the system is Windows,
 download (https://download.lfd.uci.edu/pythonlibs/archived/libraqm-0.7.1.dll.zip),
 then put 'libraqm.dll' and 'fribidi-0.dll' into 'C:\Windows\System32',
-then reboot the python kernel.
+then reinstall pillow.
 
 '''
 
